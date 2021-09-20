d7ebd42195a595d7b35196c92213c1051f7d9d65
mohsen <mohsen.mr46@gmail.com>
2021-09-20 18:39:27 +0430

    remove some unused imports

--- bestoon/urls.py
+++ bestoon/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-# from rest_framework.urlpatterns import format_suffix_patformat_suffix_patterns
 from web import views
 
 urlpatterns = [
--- web/views.py
+++ web/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import get_list_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 from .models import Employees
 from .serializers import employeesSerializers
 
