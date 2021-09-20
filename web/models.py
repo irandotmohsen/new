4b85f3e6c9cdd3514b641b32980647abe0ee46e3
mohsen <mohsen.mr46@gmail.com>
2021-09-20 15:30:11 +0430

    Employees

--- web/models.py
+++ web/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Employees(models.Model):
+    first_name = models.CharField(max_length=300)
+    last_name = models.CharField(max_length=300)
+    emp_id = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.first_name
